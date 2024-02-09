f7dbcb3a6169de1aa39140333fadb455041b54c2
sean <seanjiangsh@gmail.com>
2024-02-09 09:49:57 +0800

    fixing e2e production test failed in CI pipeline by setup environment variable "CI=true"

--- client/cypress.config.ts
+++ client/cypress.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "cypress";
 import cypressCoverageTask from "@cypress/code-coverage/task";
 
-const isProd = process.env.ENV === "PROD";
-const baseUrl = isProd
+const isCI = !!process.env.CI;
+console.log({ cypressConfigIsCI: isCI });
+
+const baseUrl = isCI
   ? "http://localhost:5000/meeting"
   : "http://localhost:5173";
-const setupNodeEvents = isProd
+const setupNodeEvents = isCI
   ? undefined
   : (on: Cypress.PluginEvents, config: Cypress.PluginConfigOptions) => {
       cypressCoverageTask(on, config);
